53a69a73a0b624c05f266fae333d662f65d6b5a5
Antoninadan <email100@i.ua>
2018-09-30 05:24:09 +0300

    pro, lesson 6, commit 2

--- LessonPRO6/src/Main.java
+++ LessonPRO6/src/Main.java
@@ -53,11 +53,11 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 //        getCitiesSimple();
+
 //        System.out.println(getCitiesByCountry("Ukraine"));
 
         Iterator<ua.i.mail100.Article> itr = getArticle().iterator();
-
-        for (; itr.hasNext();) {
+        for (; itr.hasNext(); ) {
             ua.i.mail100.Article element = itr.next();
             System.out.println(element.toString());
         }
@@ -71,11 +71,11 @@ public class Main {
         // try-catch with resources
         // Connection -> PreparedStatement -> ResultSet = PreparedStatement.executeQuery()
 
-        try(Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "estafeta")) {
-            try(PreparedStatement st = c.prepareStatement("select * From city")) {
-                try(ResultSet set = st.executeQuery()) {
-                    while(set.next()) {
-                       System.out.println(set.getString("name"));
+        try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "estafeta")) {
+            try (PreparedStatement st = c.prepareStatement("select * From city")) {
+                try (ResultSet set = st.executeQuery()) {
+                    while (set.next()) {
+                        System.out.println(set.getString("name"));
                     }
                 }
             }
@@ -88,8 +88,8 @@ public class Main {
         List<City> cities = new ArrayList<>();
 
         // default user = "postgres"
-        try(Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "estafeta")) {
-            try(PreparedStatement st = c.prepareStatement("select * From city where country_id = (select id from country where name = ?)")) {
+        try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "estafeta")) {
+            try (PreparedStatement st = c.prepareStatement("select * From city where country_id = (select id from country where name = ?)")) {
 //                // addBatch
 //                st.addBatch("insert into ... ");
 //                st.addBatch("insert into ... ");
@@ -98,8 +98,8 @@ public class Main {
                 st.setString(1, countryName);
 //                st.setString(2, cbcklkldlk); // if more then one ?
 
-                try(ResultSet set = st.executeQuery()) {
-                    while(set.next()) {
+                try (ResultSet set = st.executeQuery()) {
+                    while (set.next()) {
                         City city = new City();
                         city.setName(set.getString("name"));
                         city.setId(set.getInt("id"));
@@ -137,4 +137,16 @@ public class Main {
     }
 
 
+    private static void createTables() throws SQLException {
+
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "estafeta")) {
+            try (PreparedStatement st = connection.prepareStatement("CREATE TABLE IF NOT EXISTS sys_user2(id SERIAL PRIMARY KEY, login varchar(255), pass varchar(255), is_admin boolean)")) {
+            }
+            try (PreparedStatement st = connection.prepareStatement("CREATE TABLE IF NOT EXISTS article2(id SERIAL PRIMARY KEY, name varchar(255), content varchar(255), date varchar(255), user_id int NOT NULL, FOREIGN KEY (user_id) REFERENCES sys_user(id))")) {
+            }
+            try (PreparedStatement st = connection.prepareStatement("insert into article(name, content, date, user_id) values('ar1', 'content1', '21.12.2017', 1))")) {
+            }
+        }
+
+    }
 }
