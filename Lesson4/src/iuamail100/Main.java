969d798bc961600fd3429a5d08956917230e87ed
Antoninadan <mail100@i.ua>
2019-02-19 12:48:02 +0200

    start, lesson 4, commit 1

---
+++ Lesson4/src/iuamail100/Main.java
@@ -0,0 +1,84 @@
+package iuamail100;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		int number = 1;
+//		
+//		while (number <= 10) {
+//			System.out.println(number);
+//			number++;
+//		}
+
+//		int number = 1;
+	
+//		do {
+//			System.out.println(number);
+//			number++;
+//		} while (number < 0);
+		
+//		for (int i=1; i<=4; i++) {
+//			System.out.println(i);
+//		}
+		
+//		for (char i='a'; i<='z'; i++) {
+//			System.out.println(i);
+//		}
+		
+//		int number = 1; // замена while
+//		for (;number<=10;) {
+//			System.out.println("yuyu");
+//			number++;
+//		}
+
+//		for (int i = 1; i <= 100; i++) {
+//			if ((i % 3 == 0) && (i % 5 == 0)) {
+//				System.out.println("spam egg");
+//			} else if (i % 3 == 0) {
+//				System.out.println("spam");
+//			} else if (i % 5 == 0) {
+//				System.out.println("egg");
+//			} else {
+//				System.out.println(i);
+//			}
+//		}
+
+//		int h = 9;
+//		int w = 7;
+		
+//		for (int i = 0; i <= h; i++) {
+//			for (int j = 0; j <= w; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		for (int i = h; i >= 0; i--) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		Scanner sc = new Scanner(System.in);
+//		int h = sc.nextInt();
+//
+//		for (int i = 1; i <= h; i++) {
+//
+//			for (int j = 1; j <= h + 1; j++) {
+//				if (j <= (h - i)) {
+//					System.out.print(" ");
+//				} else {
+//					System.out.print("*");
+//				}
+//			}
+//
+//			System.out.println();
+//		}
+		
+	}
+
+}
