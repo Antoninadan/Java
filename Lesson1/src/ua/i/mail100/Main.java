21985b2832dc7fbe92f675ed6747404ea3760d87
Antoninadan <mail100@i.ua>
2019-03-02 21:46:35 +0200

    start, lesson1, commit 1

---
+++ Lesson1/src/ua/i/mail100/Main.java
@@ -0,0 +1,43 @@
+package ua.i.mail100;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		double myAge = 8.09 * 90;
+		int a = 0;
+		float b = 1.2F;
+		int aa = (int) 7.5;
+		double d = 0;
+		// myAge = 5;
+		System.out.println("Hello world");
+		System.out.println(778787 + "A");
+		System.out.println(778787 + 'A'); // как аски код
+
+		// a = myAge;
+
+		/*
+		 * not compile byte a = 2; a = a + 2;
+		 */
+
+		// все ок
+
+		b = b + 0.5F; // все float надо указать
+
+		System.out.println(aa);
+
+		// devide int 5/2 = 2, double 5/2.0 = 2.5
+
+		b = 9;
+		b++;
+		System.out.println(b);
+
+		b += 7;
+		System.out.println(b);
+
+		d = Math.cos(d);
+		System.out.println(d);
+
+	}
+
+}
